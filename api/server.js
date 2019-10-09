482e71e388f188b86100d875c1d4da71428a48e9
GustavoCesarSantos <gustavocs789@gmail.com>
2019-10-08 22:03:34 -0300

    Arrumando console da porta do servidor

--- api/server.js
+++ api/server.js
@@ -2,5 +2,5 @@ const app = require('./src/config/server/express');
 require('./src/config/database/mongoose');
 
 app.listen(3000, () => {
-    console.info('Servidor iniciado na porta 3000');
+    console.info('Server on in port: 3000');
 });
