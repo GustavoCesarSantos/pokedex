903dee78cc51338a5866b85bd7cc9c7ba8a6caf6
GustavoCS789 <gustavo.santos@berghem.com.br>
2019-11-29 14:13:42 -0300

    Criado validacao dos dados do usuario utilizando Joi

--- api/src/components/user/User.js
+++ api/src/components/user/User.js
@@ -9,5 +9,13 @@ module.exports = class User{
     this.date = user.date
   }
 
-
+  isValid(userData){
+    const userSchemaValidate = Joi.object({
+      name: Joi.string().min(3).max(100).required(),
+      email: Joi.string().required(),
+      password: Joi.string().min(6).max(24).required(),
+      level: Joi.string().required()
+    });
+    return userSchemaValidate.validate(userData);
+  }
 }
